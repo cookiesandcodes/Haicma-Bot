72b15b4759c4a09203b2e7e94b2fee7cd5dc1178
cookiesandcodes <senkaipulse@gmail.com>
2023-07-05 11:19:37 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ const {Client, IntentsBitField} = require('discord.js');
 const Haicma = new Client({
     intents:[
         IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.DirectMessages,
-        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildMessageReactions,
     ]
 })
 /*MTEyNTkzOTY0MTUxMDU0MzM2MQ.GSN_fH.jGHRsH3t7VdpUc-zC15NpnKPi_OGO3W6bmGCro  token*/
